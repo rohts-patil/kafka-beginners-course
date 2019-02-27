84b492819a30126bb2231b54664a1e28c6f7f25d
Rohit Patil <rohts.patil@gmail.com>
2019-02-28 02:25:43 +0530

    published hello world on topic via producer

--- src/main/java/com/tpt/rohtspatil/kafka/tutorial1/ProducerDemo1.java
+++ src/main/java/com/tpt/rohtspatil/kafka/tutorial1/ProducerDemo1.java
@@ -1,15 +1,36 @@
 package com.tpt.rohtspatil.kafka.tutorial1;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
 
 public class ProducerDemo1 {
   public static void main(String[] args) {
-    Properties properties = new Properties();
     String bootStrapServers = "127.0.0.1:9092";
-    properties.setProperty("bootstrap.servers", bootStrapServers);
-    properties.setProperty("key.serializer", StringSerializer.class.getName());
-    properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+    // create producer properties
+    Properties properties = new Properties();
+    properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServers);
+    properties.setProperty(
+        ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    properties.setProperty(
+        ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+    // create producer
+    KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+    // create producer record
+    ProducerRecord<String, String> record =
+        new ProducerRecord<String, String>("first_topic", "Hello World");
+
+    // send data async
+    producer.send(record);
+
+    // flush data
+    producer.flush();
+    producer.close();
   }
 }
