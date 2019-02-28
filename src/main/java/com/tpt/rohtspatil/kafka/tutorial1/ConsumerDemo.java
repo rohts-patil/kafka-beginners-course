a83d8192ab2806f3ad481994f55baad7b3af8f5c
Rohit Patil <rohts.patil@gmail.com>
2019-03-01 02:03:14 +0530

    consumer group demo

---
+++ src/main/java/com/tpt/rohtspatil/kafka/tutorial1/ConsumerDemo.java
@@ -0,0 +1,51 @@
+package com.tpt.rohtspatil.kafka.tutorial1;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerDemo {
+
+  public static void main(String[] args) {
+
+    String bootStrapServers = "127.0.0.1:9092";
+    String groupId = "my-fourth-application";
+    String topic = "first_topic";
+
+    Logger logger = LoggerFactory.getLogger(ConsumerDemo.class);
+
+    // create consumer config
+    Properties properties = new Properties();
+    properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServers);
+    properties.setProperty(
+        ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+    properties.setProperty(
+        ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+    properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+    properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+    // create consumer
+    KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+
+    // subscribe consumer to topics
+    consumer.subscribe(Arrays.asList(topic));
+
+    // poll for new data
+    while (true) {
+      ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+      for (ConsumerRecord record : records) {
+        logger.info("key:" + record.key() + " value:" + record.value());
+        logger.info("partition:" + record.partition() + " offset:" + record.offset());
+      }
+    }
+  }
+}
